8787f6d49365de0c9ece9c93d37491ae7e9a45fb
Jakc3311 <sawaketsu.yu@elgana.works>
2023-08-28 15:48:13 +0900

    信息返回

---
+++ common/output/output.go
@@ -0,0 +1,56 @@
+package output
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Successful 成功返回
+//
+//	@param c
+//	@param message
+func Successful(c *gin.Context, message string) {
+	c.JSON(http.StatusOK, gin.H{
+		"code":    http.StatusOK,
+		"message": message,
+	})
+}
+
+// Errorful 错误返回
+//
+//	@param c
+//	@param code
+//	@param message
+func Errorful(c *gin.Context, code int, message string) {
+	c.JSON(http.StatusBadRequest, gin.H{
+		"code":    code,
+		"message": message,
+	})
+}
+
+// Dataful 数据返回
+//
+//	@param c
+//	@param data
+func Dataful(c *gin.Context, data interface{}) {
+	c.JSON(http.StatusOK, gin.H{
+		"code":    http.StatusOK,
+		"data":    data,
+		"message": "success",
+	})
+}
+
+// Pageful 分页返回
+//
+//	@param c
+//	@param data
+//	@param count
+func Pageful(c *gin.Context, data interface{}, count int) {
+	c.JSON(http.StatusOK, gin.H{
+		"code":    http.StatusOK,
+		"data":    data,
+		"count":   count,
+		"message": "success",
+	})
+}
